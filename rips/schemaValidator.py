b52637e4bdf1ffdb1ffb6a05c5323e74dde1b9c6
gitlabs <spawan1999@gmail.com>
2019-05-23 17:02:11 +0530

    Minor fixes

--- rips/schemaValidator.py
+++ rips/schemaValidator.py
@@ -86,6 +86,7 @@ class schemaValidator(Validator):
     '''Function to check whether the bitmask given for the Extensions field in misa is valid.'''
         global xlen
         global extensions
+        # value=value['bitmask']
         val = value['base'] ^ value['value'] ^ extensions
         if(val > 0):
             self._error(field, "Extension Bitmask error.")
