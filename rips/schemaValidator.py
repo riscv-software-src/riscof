42021cedc1f5fe7004ae0cc12776e88bf2ccf856
Dracarys99 <spawan1999@gmail.com>
2019-05-24 14:54:53 +0530

    Implemented all fields of mtvec

--- rips/schemaValidator.py
+++ rips/schemaValidator.py
@@ -89,3 +89,16 @@ class schemaValidator(Validator):
             self._error(field,"0 not a valid entry as U extension is not supported.")
         if(1 in value) and extensions ^ int("0040000",16) == 0:
             self._error(field,"1 not a valid entry as S extension is not supported.")
+
+    def _check_with_mtveccheck(self,field,value):
+        '''Function to check whether the inputs in range type in mtvec are valid.'''
+        global xlen
+        maxv = 2**(xlen)-4
+        if not((value['base']<value['bound']) and value['base']<=maxv and value['bound']<=maxv):
+            self._error(field,"Invalid values.")
+    
+    def _check_with_mtvecdist(self,field,value):
+        global xlen
+
+        if max(value)>2**(xlen-2)-1:
+            self._error(field,"value cant be greater than "+str(2**(xlen)-4))
