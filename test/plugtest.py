2fa395b632d1b4856887cda6a9e2f4c25f9a9ca0
gitlabs <spawan1999@gmail.com>
2019-05-28 10:25:11 +0530

    Enabled logging and added shell command support for plugin.

--- test/plugtest.py
+++ test/plugtest.py
@@ -1,20 +1,30 @@
 import oyaml as yaml
 import importlib
+import common.utils
+import logging
 # import plugin.fromSchema as module
 def kk():
     print('k')
 
 def test():
+    # Set up the logger
+    common.utils.setup_logging("debug")
+    logger = logging.getLogger()
+    logger.handlers = []
+    ch = logging.StreamHandler()
+    ch.setFormatter(common.utils.ColoredFormatter())
+    logger.addHandler(ch)
+
     file = open("test/test.yaml","r")
     schema_yaml = yaml.safe_load(file)
     module = importlib.import_module('plugin.fromSchema')
-    my_class = getattr(module, 'fromSchema')
+    my_class = getattr(module, 'model_from_yaml')
     my_instance = my_class()
 
     my_instance.initialise(schema_yaml)
     my_instance.presim("I-SB-01")
     my_instance.execute("I-SB-01"," -DTEST_PART_1=True")
-    my_instance.postsim("I-SB-01")
+    my_instance.postsimshell("I-SB-01")
 
 if __name__ == '__main__':
     test()
