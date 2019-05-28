555b7882b543eb9e175d56a5cc1c02ea5252bc45
Dracarys99 <spawan1999@gmail.com>
2019-05-28 16:29:56 +0530

    Removed readonly from all fields and updated documentation.

--- rips/schemaValidator.py
+++ rips/schemaValidator.py
@@ -62,6 +62,12 @@ class schemaValidator(Validator):
         if value > (2**xlen)-1:
             self._error(field, "Max value is greater than "+str(2**xlen-1))
 
+    def _check_with_max_length_64(self,field,value):
+        '''Function to check whether the given value is less than the maximum value that can be stored(2^64-1) for 
+        registers which are 64 bits wide irrespective of XLEN.'''
+        if value > (2**64)-1:
+            self._error(field, "Max value is greater than "+str(2**64-1))
+    
     def _check_with_len_check(self, field, value):
         '''Function to check whether the given value is less than XLEN/32(For check).'''
         global xlen
@@ -124,6 +130,12 @@ class schemaValidator(Validator):
         '''Function to check whether the inputs in range type in WARL fields are valid.'''
         global xlen
         maxv = 2**(xlen)-1
+        if not((value['base']<value['bound']) and value['base']<=maxv and value['bound']<=maxv):
+            self._error(field,"Invalid values.")
+
+    def _check_with_rangecheck_64(self,field,value):
+        '''Function to check whether the inputs in range type in 64 bit WARL fields are valid.'''
+        maxv = 2**64-1
         if not((value['base']<value['bound']) and value['base']<=maxv and value['bound']<=maxv):
             self._error(field,"Invalid values.")
     
