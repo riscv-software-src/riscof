a7c935ed7ffab8915fd36b714d8d083face7faf3
gitlabs <spawan1999@gmail.com>
2019-05-30 13:03:34 +0530

    Finished 2 model based framework. (Spike vs Spike)

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ The work here is under development and is not stable for consumption .. yet!
 ## Documentation
 You can find the latest documentation of the work: [here](https://riscof.readthedocs.io/en/latest/)
 
+Sample command for framework (under development)
+
+'''
+python3 -m framework.main -bm model_from_yaml -bf Examples/template_env.yaml -df Examples/template_env.yaml -dm model_from_yaml -ispec Examples/template_isa_checked.yaml -pspec Examples/template_platform_checked.yaml --verbose debug
+
+python3 -m framework.main -bm model_from_yaml -bf Examples/template_env_2.yaml -df Examples/template_env.yaml -dm model_from_yaml -ispec Examples/template_isa_checked.yaml -pspec Examples/template_platform_checked.yaml --verbose debug
+'''
--- framework/plugtest.py
+++ framework/plugtest.py
@@ -2,6 +2,8 @@ import oyaml as yaml
 import importlib
 import common.utils as utils
 import logging
+import shutil
+import os
 
 def test():
     # Set up the logger
@@ -17,12 +19,22 @@ def test():
     my_class = getattr(module, 'model_from_yaml')
     my_instance = my_class()
 
-    schema = "Examples/template_env.yaml"
-    my_instance.initialise_from_file(schema)
-    my_instance.compile("I-SB-01"," -DTEST_PART_1=True")
-    my_instance.presim("I-SB-01")
-    my_instance.simulate("I-SB-01")
-    my_instance.postsim("I-SB-01")
+    work_dir = os.getcwd()+"/work/"
+    suite = os.getcwd()+"/suite/"
+    #Creating work directory
+    if not os.path.exists(work_dir):
+        logger.debug('Creating new work directory: '+work_dir)
+        os.mkdir(work_dir)
+    else:
+        logger.debug('Removing old work directory: '+work_dir)
+        shutil.rmtree(work_dir)
+        logger.debug('Creating new work directory: '+work_dir)
+        os.mkdir(work_dir)
 
+    schema = "Examples/template_env.yaml"
+    my_instance.initialise_from_file(schema,work_dir=work_dir,suite=suite)
+    my_instance.compile("I-SB-01"," -DTEST_PART_1=True")
+    k = my_instance.simulate("I-SB-01")
+    print(k)
 if __name__ == '__main__':
     test()
