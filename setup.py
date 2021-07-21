7d62bc284df08deb637f22a9af828738601862ae
Muhammad Bilal Sakhawat <bilal.sakhawat@10xengineers.ai>
2021-07-21 12:28:14 +0500

    Bump version: 1.21.0 → 1.21.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_requirements = [ ]
 test_requirements = [ ]
 
 setup(name="riscof",
-      version='1.21.0',
+      version='1.21.1',
       description="RISC-V Architectural Test Framework",
       long_description=readme + '\n\n',
       classifiers=[
