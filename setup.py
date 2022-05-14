f5dc9164550b8fec9854c0c55313baaca5d21350
Neel Gala <neelgala@incoresemi.com>
2022-05-14 14:30:14 +0530

    Bump version: 1.23.4 → 1.24.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_requirements = [ ]
 test_requirements = [ ]
 
 setup(name="riscof",
-      version='1.23.4',
+      version='1.24.0',
       description="RISC-V Architectural Test Framework",
       long_description=readme + '\n\n',
       classifiers=[
