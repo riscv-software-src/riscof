def006e4b0b19826a4efcc80827ce8ffa78698d0
Priyansh Rathi <techiepriyansh@gmail.com>
2022-08-27 17:36:53 +0530

    Bump version: 1.24.1 → 1.24.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_requirements = [ ]
 test_requirements = [ ]
 
 setup(name="riscof",
-      version='1.24.1',
+      version='1.24.2',
       description="RISC-V Architectural Test Framework",
       long_description=readme + '\n\n',
       classifiers=[
